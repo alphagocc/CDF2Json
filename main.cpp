f83dd60d5b32c053569ca4c30090cb360d62e33a
Alphagocc <alphagocc@163.com>
2020-11-30 02:31:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,6 @@ int main(int argc, char *argv[])
     //QCoreApplication a(argc, argv);
     Transform t;
     qDebug() << argc;
-    if (argc == 1)
-    {
-        t.doit("C:\\Users\\Alphagocc\\Desktop\\Code\\20170902p\\2017-09-02.cdf", "aaaaaa.json");
-        return 0;
-    }
     t.doit(argv[1], argv[2]);
     return 0;
 }
